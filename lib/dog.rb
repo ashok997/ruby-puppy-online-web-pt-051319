0639b3bebc9a9326c956f4a8ebccc841d70794b5
ASHOK MAHARJAN <ashok997@gmail.com>
2019-06-04 16:17:08 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,8 +1,20 @@
 class Dog
   @@ all =[]
   
+  attr_accessor :name
+ 
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+ 
+  def self.all
+    @@all
+  end
   
-  
+  def self.clear.all
+    
+  end
   
   
 end
